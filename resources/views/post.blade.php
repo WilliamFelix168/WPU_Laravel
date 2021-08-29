878552bf047eeba4b7202bf45b2621e24d99b985
William Felix <william.felix059@gmail.com>
2021-08-29 19:56:11 +0700

    testing done

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,15 +1,17 @@
 @extends('layouts.main')
 
 @section('container')
-    <article>
-        <article class="mb-5">
-            <h2>
-                {{ $post->title }}
-            </h2>
-            {!! $post->body !!}
-        </article>
-    </article>
+<article>
+    <article class="mb-5">
+        <h2>
+            {{ $post->title }}
+        </h2>
 
-    <a href="/posts">Back to Post</a>
-    
+        <p>By . William Felix in <a href="/categories /{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        {!! $post->body !!}
+    </article>
+</article>
+
+<a href="/posts">Back to Post</a>
+
 @endsection
