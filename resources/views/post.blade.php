eee4e04651e42954a2f2e0362a58e09c14e51464
William Felix <william.felix059@gmail.com>
2021-08-29 20:31:32 +0700

    video ke 8 done

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -7,7 +7,7 @@
             {{ $post->title }}
         </h2>
 
-        <p>By . William Felix in <a href="/categories /{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <p>By . William Felix in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
         {!! $post->body !!}
     </article>
 </article>
